269f13ad7675de72a97d197a0e73d71527f5b6b4
aktiger <uestczhangchao@gmail.com>
2018-05-18 17:31:11 +0800

    get work group size

--- cmake-build-debug/test.cl
+++ cmake-build-debug/test.cl
@@ -9,11 +9,28 @@
            ulong n,
            global const double *a,
            global const double *b,
-           global double *c
+           global double *c,
+           global size_t *global_id
            )
     {
         size_t i = get_global_id(0);
+
+         //get_local_size(0) * get_num_groups(0)
+        //*global_id = get_global_id(1025);
+
+        *global_id = get_local_size(0);
+        // *global_id = get_num_groups(0);
+
         if (i < n) {
            c[i] = a[i] + b[i];
         }
+
+
     };
+
+    kernel void readInfo(global size_t *info) {
+        //*info =  get_global_id(10) ;
+         *info =  124;
+
+
+    }
--- hello.cpp
+++ hello.cpp
@@ -32,11 +32,10 @@ static const char source[] =
 
 
 
-
-
-
 int main() {
-    const size_t N = 1 << 20;
+    const size_t N = (1 << 8) + 250 ;
+
+    std:: cout << "N is " <<  N << std::endl;
     bool profile=true;
     bool block = false;
     cl_int  status = 0;
@@ -130,11 +129,14 @@ int main() {
 	}
 
 	cl::Kernel add(program, "add");
+	cl::Kernel readInfo(program, "readInfo");
 
 	// Prepare input data.
 	std::vector<double> a(N, 1);
 	std::vector<double> b(N, 2);
 	std::vector<double> c(N);
+	std::size_t info;
+	size_t global_id =0.f;
 
 
 	// Allocate device buffers and transfer input data to device.
@@ -147,20 +149,45 @@ int main() {
 	cl::Buffer C(context, CL_MEM_READ_WRITE,
 		c.size() * sizeof(double));
 
+
+	cl::Buffer Info(context, CL_MEM_READ_WRITE, sizeof(info));
+    std::cout << "sizeof(info) is " << sizeof(info) << std::endl;
+
+    cl::Buffer Global_id(context, CL_MEM_READ_WRITE,  sizeof(size_t));
+
 	// Set kernel parameters.
 	add.setArg(0, static_cast<cl_ulong>(N));
 	add.setArg(1, A);
 	add.setArg(2, B);
 	add.setArg(3, C);
+	add.setArg(4, Global_id);
+
+	readInfo.setArg(0, Info);
 	
 	// Launch kernel on the compute device.
 	queue.enqueueNDRangeKernel(add, cl::NullRange, N, cl::NullRange);
+    queue.enqueueNDRangeKernel(readInfo,cl::NullRange,1, cl::NullRange);
+
+    size_t group_size ;
+    size_t ret;
+
+    clGetKernelWorkGroupInfo(add.operator()(), device[0].operator()(), CL_KERNEL_WORK_GROUP_SIZE, sizeof(size_t),&group_size, &ret);
+
 
 	// Get result back to host.
 	queue.enqueueReadBuffer(C, CL_TRUE, 0, c.size() * sizeof(double), c.data());
+	queue.enqueueReadBuffer(Global_id, CL_TRUE,0, sizeof(size_t) , &global_id);
+
+    queue.enqueueReadBuffer(Info, CL_TRUE,0, sizeof(info) , &info);
+
+
+
 
 	// Should get '3' here.
 	std::cout << c[42] << std::endl;
+	std::cout << "info is :" << info << std::endl;
+    std::cout << "global_id is :" << global_id << std::endl;
+
     } catch (const cl::Error &err) {
 	std::cerr
 	    << "OpenCL error: "
